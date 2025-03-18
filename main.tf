c477ffec5e4d9ba74ce30823a7e14355844ee83f
SvetaYastrebkova <sunsvets@gmail.com>
2025-03-18 22:01:52 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,37 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 6, k)]
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 6, k + 10)]
+
+  enable_nat_gateway   = true
+  create_igw           = true
+  enable_dns_hostnames = true
+  single_nat_gateway   = true
+
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"              = 1
+    "kubernetes.io/cluster/${local.name}" = "shared" #adding tags for deploy elb
+
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"     = 1
+    "kubernetes.io/cluster/${local.name}" = "shared" #adding tags for deploy elb
+    "karpenter.sh/discovery"              = local.name
+  }
+  
+  tags = local.tags
+}
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+aws_region       = "us-east-1"
+environment_name = "growing_startup"
+vpc_cidr         = "172.16.0.0/16"
+cluster_version  = "1.31"
+node_group_name  = "managed-node"
